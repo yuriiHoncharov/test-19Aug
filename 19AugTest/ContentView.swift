c6e5296a54b67d44c83d855f0dfbf23025484476
Yurii Honcharov <y.honcharov@lampalampa.net>
2022-08-19 15:25:36 +0300

    firstComit

---
+++ 19AugTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  19AugTest
+//
+//  Created by Yurii Honcharov on 19.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 19AugTest/_9AugTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9AugTestApp.swift
+//  19AugTest
+//
+//  Created by Yurii Honcharov on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct _9AugTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
